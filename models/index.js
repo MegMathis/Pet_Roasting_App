1d829e3bfd81a66edb2d672003aea10451513e87
jamessciacca <noing2003@gmail.com>
2023-04-23 16:11:33 -0400

    added routes and Post sql table

--- models/index.js
+++ models/index.js
@@ -1,20 +1,9 @@
 const Post = require("./Post");
-const Profile = require("./Profile");
 const User = require("./User");
 const Comment = require("./Comment");
 
-
-User.hasOne(Profile, {
-  foreignKey: "user_id",
-});
-
-Profile.belongsTo(User, {
-  foreignKey: "user_id",
-});
-
 module.exports = {
   Post,
-  Profile,
   User,
   Comment,
 };
